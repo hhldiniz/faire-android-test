2a6fd4656cfe6412aad77353a1766688986d1a3b
Hugo <hhldiniz@gmail.com>
2022-08-29 20:36:19 -0300

    Adding fragment that will hold forecast info

--- app/src/main/java/com/hugo/fairecast/app/view/activities/MainActivity.kt
+++ app/src/main/java/com/hugo/fairecast/app/view/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hugo.fairecast
+package com.hugo.fairecast.app.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hugo.fairecast.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
